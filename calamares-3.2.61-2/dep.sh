0931e0f7fffe29e251552f7ff0624a9031efe9b4
aamirali51 <62554127+aamirali51@users.noreply.github.com>
2024-10-20 23:35:12 +0500

    upda

--- calamares-3.2.61-2/dep.sh
+++
@@ -1,6 +0,0 @@
-# Installing calamares dependencies for ALG
-
-echo "Installing: kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore mkinitcpio-openswap ckbcomp hwinfo qt6-svg polkit-qt6 gtk-update-icon-cache squashfs-tools libpwquality pybind11 python extra-cmake-modules qt6-tools qt6-translations git"
-
-sudo pacman -Sy kconfig kcoreaddons kiconthemes ki18n kio solid yaml-cpp kpmcore mkinitcpio-openswap ckbcomp hwinfo qt6-svg polkit-qt6 gtk-update-icon-cache squashfs-tools libpwquality pybind11 python extra-cmake-modules qt6-tools qt6-translations git
-
